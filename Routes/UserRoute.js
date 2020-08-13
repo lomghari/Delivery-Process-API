a8466e723ad287522aa8a31561eef81fb9ff5d26
Kossaby-Dev <yassine.kossaby.work@gmail.com>
2020-08-13 19:33:16 +0100

    Fix bug Nu3

--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -8,7 +8,7 @@ RouteUser.route("/login").post(AuthControle.LogIn);
 RouteUser.route("/user").get(AuthControle.Checker,UserControle.getUser).patch(AuthControle.Checker,UserControle.UpdateUser).delete(AuthControle.Checker,UserControle.DeleteUser)
 RouteUser.route("/active").patch(AuthControle.Checker,UserControle.ActiveUser)
 RouteUser.route("/desactive").patch(AuthControle.Checker,UserControle.DesactiveUser)
-RouteUser.route('/').get(AuthControle.Checker,UserControle.getAllUser).put(AuthControle.Checker, UserControle.UpdateUsers)
+RouteUser.route('/').get(AuthControle.Checker,UserControle.getAllUser).patch(AuthControle.Checker, UserControle.UpdateUsers)
 RouteUser.route("/createuser").post(AuthControle.Checker,UserControle.CreateUser)
 
 
