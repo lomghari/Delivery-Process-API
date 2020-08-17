c23b45f887628fdf9bd4adce569e7ffd5a0fd19f
Kossaby-Dev <yassine.kossaby.work@gmail.com>
2020-08-17 16:06:32 +0100

    fix bug database

--- Util/Database.js
+++ Util/Database.js
@@ -3,4 +3,4 @@ const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize('admin_app','admin_app','123456x',{dialect:'mysql',  host: 'foorsa.ma'});
 const sequelizeLocal = new Sequelize('gestion_app','root','mamak',{dialect:'mysql',  host: 'localhost'});
-module.exports = sequelizeLocal;
+module.exports = sequelize;
